6562f51b3ba03c96ecefa52d7b85cdb23864f674
4d55397500 <4d55397500@users.noreply.github.com>
2020-04-30 14:09:56 -0700

    indices

--- db.py
+++ db.py
@@ -10,31 +10,42 @@ NCOLUMNS = 5
 NROWS = 10**2
 
 
-class Node(object):
-    def __init__(self, value, row, left, right):
-        self.value = value
-        self.row = row
-        self.left = left
-        self.right = right
-
-
 class BinarySearchTree(object):
-
     def __init__(self):
-        pass
+        self.value = None
+        self.row = None
+        self.left = None
+        self.right = None
 
-    def add(self, value):
-        pass
+    def add(self, value, row):
+        if self.value is None:
+            self.value = value
+            self.row = row
+            self.left = BinarySearchTree()
+            self.right = BinarySearchTree()
+        elif value < self.value:
+            self.left.add(value, row)
+        else:
+            self.right.add(value, row)
+
+    def print_tree(self, parent, is_left):
+        # breadth-first print
+        if self.value is not None:
+            if is_left:
+                print(f'{self.value}, left child of {parent}. rowptr to {self.row}')
+                assert parent is None or self.value < parent
+            else:
+                print(f'{self.value}, right child of {parent}. rowptr to {self.row}')
+                assert parent is None or self.value > parent
+            self.left.print_tree(self.value, True)
+            self.right.print_tree(self.value, False)
 
 
 class Database(object):
-
     """ A database modeled as a linked list of rows """
-
     def __init__(self):
         self.row_value = [None] * NCOLUMNS
         self.next = None
-
     def add_entry(self, row, column, entry):
         current = self
         i = 0
@@ -44,7 +55,6 @@ class Database(object):
             current = current.next
             i += 1
         current.row_value[column] = entry
-
     def print_db(self):
         current = self
         i = 0
@@ -61,8 +71,31 @@ def fill_sample_database():
     for i in range(nrows):
         for j in range(ncols):
             database.add_entry(i, j, strgen())
-    database.print_db()
+    #database.print_db()
+    return database
+
+
+def build_indices(database, cols):
+    # build indices over specified columns
+    print('building indices ...')
+    current = database
+    indices = [BinarySearchTree() for _ in cols]
+    while current is not None:
+        for j, col in enumerate(cols):
+            bst = indices[j]
+            entry = current.row_value[col]
+            bst.add(entry, current)
+        current = current.next
+    print('finished building indices.')
+    for index in indices:
+        index.print_tree(None, None)
+
+
+
+def main():
+    database = fill_sample_database()
+    build_indices(database, cols=[0, 2])
 
 
 if __name__ == "__main__":
-    fill_sample_database()
+    main()
